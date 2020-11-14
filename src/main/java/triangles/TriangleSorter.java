71f43aea30b6395eaabe943a41ee5dffb540768b
Lauren Monowar-Jones <lvjones214@aol.com>
2020-11-14 15:26:46 -0500

    refactored and pass first test

--- src/main/java/triangles/TriangleSorter.java
+++ src/main/java/triangles/TriangleSorter.java
@@ -2,7 +2,7 @@ package triangles;
 
 public class TriangleSorter {
     public String analyze(int sideA, int sideB, int sideC) {
-        if(sideA == 3 && sideB == 3 && sideC ==3){
+        if(sideA ==  sideB  && sideB == sideC){
             return "Equilateral";
         }
         return "";
